e41fb71f2ccdcc4d6b739a9d33427ee4ef91b573
josiahcameron <josiahcameron2015@gmail.com>
2023-01-10 08:41:40 -0500

    Exercise 6

--- exercises.js
+++ exercises.js
@@ -129,8 +129,20 @@ const noDupes = function(array){
 
 // Put your answer below -------------------------
 
+const twinCheck = (array1, array2) =>{
+    const arr1 = array1;
+    const arr2 = array2;
+    if (arr1 === arr2) return true;
+    if (arr1 == null || arr2 == null) return false;
+    if (arr1.length !== arr2.length) return false;
+    
+    for (i = 0; i < arr1.length; i++){
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true
+}
 
-
+twinCheck([1,2,3,4], [1,2,3,4])
 
 
 
