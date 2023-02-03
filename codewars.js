32dba8a4940de14ffb902055c06136cc75b3bf26
josiahcameron <josiahcameron2015@gmail.com>
2023-02-03 13:08:08 -0500

    First Two Codewars

--- codewars.js
+++ codewars.js
@@ -1,39 +1,32 @@
 // # 1
 // Testing 1-2-3
 // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
-var number=function(array){
-    const initArray = array;
-    const newArray = [];
-    const initNum = 0;
-    for (i = 0; i < initArray.length; i++){
-      let initNum++
-      newArray.push(`${initNum}: ${initArray[i]}`);
-      }
-    return newArray
+function number(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(`${i + 1}: ${array[i]}`);
   }
-
-
+  return newArray;
+}
 
 // # 2
 // Filling an array (part 1)
 // https://www.codewars.com/kata/571d42206414b103dc0006a1/train/javascript
-
-
-
+function arr(n) {
+  const array = [];
+  for (let i = 0; i < n; i++) {
+    array.push(i);
+  }
+  return array;
+}
 
 // # 3
 // Drone Fly-By
 // https://www.codewars.com/kata/58356a94f8358058f30004b5/train/javascript
 
-
-
-
 // # 4
 // Get the mean of an array
 // https://www.codewars.com/kata/563e320cee5dddcf77000158/train/javascript
-
-
-
 
 // #5
 // THE SOUL OF WIT: REVERSE AN ARRAY
